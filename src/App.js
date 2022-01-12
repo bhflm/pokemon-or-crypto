38c99fa96d6e7ed39b53fe3f250256afe6224637
German Bonin <germanbonin@gmail.com>
2022-01-12 15:01:09 -0300

    Add: basic styling and stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="App-main">
+        <div className="title">
+          <h1 className="title-text">Crypto or Pokemon?</h1>
+        </div>
+        <div className="home">
+          <div className="question">
+            <p className="question-text">Alakazam</p>
+          </div>
+        <div className="options">
+          <button className="button">
+            <p className="button-text">Crypto</p>
+          </button>
+          <button className="button">
+            <p className="button-text">Pokemon</p>
+          </button>
+        </div> 
+        </div>
+        <div className="bottom-bar">
+        <p className="bottom-bar-text">
+        Made by <a className="bottom-bar-link" href="https://twitter.com/germibh/">grmi.eth</a>, 
+        inspired by <a className="bottom-bar-link" href="https://pixelastic.github.io/pokemonorbigdata/">this idea</a>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>
+      </div>
     </div>
   );
 }
